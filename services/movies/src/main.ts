73394f89f996013f3a6d1a794087921e426c173d
Jakub Stolarski <gamernice223@gmail.com>
2022-04-29 23:34:44 +0200

    Add ValidationPipe

--- services/movies/src/main.ts
+++ services/movies/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "./config/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
 
     const swaggerConfig = new DocumentBuilder()
         .addSecurity("bearer", {
