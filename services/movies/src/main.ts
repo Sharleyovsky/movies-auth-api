58ee9c9f33569821d6d377e120ed3ff056699288
Jakub Stolarski <gamernice223@gmail.com>
2022-04-29 22:35:33 +0200

    Add swagger

--- services/movies/src/main.ts
+++ services/movies/src/main.ts
@@ -1,9 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 import { config } from "./config/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(config.port);
+    const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .addSecurity("basic", {
+            type: "http",
+            scheme: "basic",
+        })
+        .setTitle("Movies API")
+        .setVersion("0.1")
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+    SwaggerModule.setup("swagger", app, document);
+    await app.listen(config.port);
 }
 bootstrap();
