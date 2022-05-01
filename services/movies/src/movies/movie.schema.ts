5db2776cf11f68d19c0f62049ce357a86bdfabff
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 07:04:48 +0200

    Add movie.schema.ts

---
+++ services/movies/src/movies/movie.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Movie {
+    @Prop({ required: true })
+    userId: number;
+
+    @Prop({ required: true })
+    title: string;
+
+    @Prop({ required: true })
+    released: Date;
+
+    @Prop({ required: true })
+    genre: string;
+
+    @Prop({ required: true })
+    director: string;
+}
+
+export type MovieDocument = Movie & Document;
+export const MovieSchema = SchemaFactory.createForClass(Movie);
