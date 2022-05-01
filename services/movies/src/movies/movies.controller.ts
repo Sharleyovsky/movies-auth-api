72f32f8c88e3b238c50117721e6271887063c6a8
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 14:48:34 +0200

    Add new Post endpoint

--- services/movies/src/movies/movies.controller.ts
+++ services/movies/src/movies/movies.controller.ts
@@ -1,17 +1,41 @@
-import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    UseGuards,
+    Request,
+    Post,
+    Body,
+} from "@nestjs/common";
 import { JwtGuard } from "../auth/guards/jwt.guard";
 import { User } from "../types/User";
 import { UserRequest } from "../types/UserRequest";
 import { MoviesService } from "./movies.service";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { MovieDto } from "./dto/movie.dto";
 
+@ApiTags("movies")
 @Controller("movies")
 export class MoviesController {
     constructor(private readonly moviesService: MoviesService) {}
 
     @UseGuards(JwtGuard)
     @Get()
+    @ApiResponse({
+        status: 200,
+        description: "Get all movies created by a authenticated user",
+        isArray: true,
+    })
     async getMovies(@Request() req: UserRequest) {
         const { id }: User = req.user;
         return await this.moviesService.findAll(id);
+    }
+
+    @UseGuards(JwtGuard)
+    @Post()
+    async createMovie(
+        @Request() req: UserRequest,
+        @Body() { title }: MovieDto,
+    ) {
+        return await this.moviesService.create({ title, id: req.user.id });
     }
 }
