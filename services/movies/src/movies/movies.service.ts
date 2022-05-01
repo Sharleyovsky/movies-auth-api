b5367a3c8a26a5cabd30dd8fac85ddb80b34f36d
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 14:48:11 +0200

    Add create method

--- services/movies/src/movies/movies.service.ts
+++ services/movies/src/movies/movies.service.ts
@@ -2,13 +2,28 @@ import { Model } from "mongoose";
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Movie, MovieDocument } from "./movie.schema";
+import { OmdbService } from "../omdb/omdb.service";
 
 @Injectable()
 export class MoviesService {
     constructor(
         @InjectModel(Movie.name)
         private readonly movieModel: Model<MovieDocument>,
+        private readonly omdbService: OmdbService,
     ) {}
+
+    async create({ title, id }: { title: string; id: number }) {
+        const { Title, Released, Genre, Director } =
+            await this.omdbService.getMovie(title);
+
+        return new this.movieModel({
+            userId: id,
+            title: Title,
+            released: Released,
+            genre: Genre,
+            director: Director,
+        }).save();
+    }
 
     async findAll(id: number) {
         return this.movieModel.find({ userId: id }).exec();
