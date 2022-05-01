da50dc5b729a0d4598834aae6257e055a92c8f03
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 07:52:52 +0200

    Add model and findAll method

--- services/movies/src/movies/movies.service.ts
+++ services/movies/src/movies/movies.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Model } from "mongoose";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Movie, MovieDocument } from "./movie.schema";
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+    constructor(
+        @InjectModel(Movie.name)
+        private readonly movieModel: Model<MovieDocument>,
+    ) {}
+
+    async findAll(id: number) {
+        return this.movieModel.find({ userId: id }).exec();
+    }
+}
