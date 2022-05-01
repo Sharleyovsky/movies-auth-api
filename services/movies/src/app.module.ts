bb59424f38e77c661a53790449f17bbe055656b0
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 04:55:29 +0200

    Add auth module

--- services/movies/src/app.module.ts
+++ services/movies/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service";
 import { MoviesController } from "./movies/movies.controller";
 import { MoviesService } from "./movies/movies.service";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [ConfigModule.forRoot(), AuthModule],
     controllers: [AppController, MoviesController],
     providers: [AppService, MoviesService],
 })
