7ff20af4dd8468db33696625d082b89e33e691d6
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 02:26:44 +0200

    Add ConfigModule to the imports

--- services/movies/src/app.module.ts
+++ services/movies/src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MoviesController } from "./movies/movies.controller";
+import { MoviesService } from "./movies/movies.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, MoviesService],
+    imports: [ConfigModule.forRoot()],
+    controllers: [AppController, MoviesController],
+    providers: [AppService, MoviesService],
 })
 export class AppModule {}
