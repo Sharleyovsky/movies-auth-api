e66cf41b76d3c39384672abc78ae6bb268434ae0
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 07:53:07 +0200

    Add get endpoint

--- services/movies/src/movies/movies.controller.ts
+++ services/movies/src/movies/movies.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import { JwtGuard } from "../auth/guards/jwt.guard";
+import { User } from "../types/User";
+import { UserRequest } from "../types/UserRequest";
+import { MoviesService } from "./movies.service";
 
-@Controller('movies')
-export class MoviesController {}
+@Controller("movies")
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @UseGuards(JwtGuard)
+    @Get()
+    async getMovies(@Request() req: UserRequest) {
+        const { id }: User = req.user;
+        return await this.moviesService.findAll(id);
+    }
+}
