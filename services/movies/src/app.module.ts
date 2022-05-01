2e4bc9d6017707583654a74b4cd18ebcc3a3de12
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 06:25:31 +0200

    Revert "remove ConfigModule"
    This reverts commit 450dab8b858ef8543995e54b48ac5810f39b43af.

--- services/movies/src/app.module.ts
+++ services/movies/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MoviesController } from "./movies/movies.controller";
 import { MoviesService } from "./movies/movies.service";
+import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { config } from "./config/config";
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         MongooseModule.forRoot(config.connectionUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
