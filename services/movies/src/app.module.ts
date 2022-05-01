16741f149d5e8bc50d80c8975038efc138b915b5
Jakub Stolarski <gamernice223@gmail.com>
2022-05-01 05:16:42 +0200

    Add MongooseModule to the imports

--- services/movies/src/app.module.ts
+++ services/movies/src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from "./app.service";
 import { MoviesController } from "./movies/movies.controller";
 import { MoviesService } from "./movies/movies.service";
 import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { config } from "./config/config";
 
 @Module({
-    imports: [ConfigModule.forRoot(), AuthModule],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(config.connectionUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        }),
+        AuthModule,
+    ],
     controllers: [AppController, MoviesController],
     providers: [AppService, MoviesService],
 })
