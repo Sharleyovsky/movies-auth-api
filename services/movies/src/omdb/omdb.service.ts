873176d9c6b22d707312644914260bdd526a946c
Jakub Stolarski <gamernice223@gmail.com>
2022-05-06 21:38:22 +0200

    Add error handling

--- services/movies/src/omdb/omdb.service.ts
+++ services/movies/src/omdb/omdb.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+    HttpException,
+    HttpStatus,
+    Injectable,
+    InternalServerErrorException,
+} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import axios from "axios";
 
@@ -12,9 +17,47 @@ export class OmdbService {
     async getMovie(title: string) {
         try {
             const { data } = await axios.get(`${this.url}&t=${title}`);
-            return data;
-        } catch {
-            throw new BadRequestException();
+
+            if (data?.Response !== "False") {
+                return data;
+            }
+
+            if (data?.Error === "Incorrect IMDb ID.") {
+                throw new HttpException(
+                    {
+                        statusCode: HttpStatus.BAD_REQUEST,
+                        message: "Title is either empty or incorrect!",
+                    },
+                    HttpStatus.BAD_REQUEST,
+                );
+            }
+
+            if (data?.Error === "Movie not found!") {
+                throw new HttpException(
+                    {
+                        statusCode: HttpStatus.NOT_FOUND,
+                        message:
+                            "Movie that you are looking for wasn't found in the OMDB",
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }
+        } catch (error) {
+            if (error?.response.status === 401) {
+                throw new HttpException(
+                    {
+                        statusCode: HttpStatus.BAD_GATEWAY,
+                        message: "Server is using invalid API key!",
+                    },
+                    HttpStatus.BAD_GATEWAY,
+                );
+            }
+
+            if (error?.response && error?.status) {
+                throw new HttpException(error.response, error.status);
+            }
+
+            throw new InternalServerErrorException();
         }
     }
 }
