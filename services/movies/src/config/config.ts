974acaf2682727d1ee42f7a5ae1458f578fc63af
Jakub Stolarski <gamernice223@gmail.com>
2022-05-06 18:07:32 +0200

    Add env validation

--- services/movies/src/config/config.ts
+++ services/movies/src/config/config.ts
@@ -1,9 +1,20 @@
-export const getConfig = () => ({
-    port: process.env.MOVIES_PORT ?? 3000,
-    title: "MOVIES API",
-    version: "0.1",
-    jwt: process.env.JWT_SECRET,
-    connectionUri: process.env.MONGO_URI,
-    omdbUrl: `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}`,
-    apiCallsLimit: 5,
-});
+export const getConfig = () => {
+    const requiredEnvs = ["JWT_SECRET", "MONGO_URI", "OMDB_API_KEY"];
+    requiredEnvs.every((env) => {
+        if (!Object.keys(process.env).includes(env)) {
+            throw new Error(`Environment variable ${env} is not defined!`);
+        }
+
+        return true;
+    });
+
+    return {
+        port: process.env.MOVIES_PORT ?? 3000,
+        title: "MOVIES API",
+        version: "0.1",
+        jwt: process.env.JWT_SECRET,
+        connectionUri: process.env.MONGO_URI,
+        omdbUrl: `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}`,
+        apiCallsLimit: 5,
+    };
+};
