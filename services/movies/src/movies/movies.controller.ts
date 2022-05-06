eabec2cddab341a5053e93316e270cb2b08a7c27
Jakub Stolarski <gamernice223@gmail.com>
2022-05-07 00:16:06 +0200

    Add new movies/:id endpoint

--- services/movies/src/movies/movies.controller.ts
+++ services/movies/src/movies/movies.controller.ts
@@ -4,6 +4,8 @@ import {
     Get,
     HttpException,
     HttpStatus,
+    InternalServerErrorException,
+    Param,
     Post,
     Request,
     UseGuards,
@@ -41,6 +43,68 @@ export class MoviesController {
     })
     async getMovies(@Request() req: UserRequest) {
         return await this.moviesService.findUserMovies(req.user.id);
+    }
+
+    @UserRoles(UserRoleEnum.Basic, UserRoleEnum.Premium)
+    @UseGuards(JwtGuard, UserRolesGuard)
+    @Get(":id")
+    @ApiBearerAuth()
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: "Get all movies created by a authenticated user",
+        isArray: true,
+        type: MovieDto,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: "Calling API without authorization header",
+        type: ErrorDto,
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: "Movie was not present in the database",
+        type: ErrorDto,
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description: "Movie ID was not type of ObjectId used in MongoDB",
+        type: ErrorDto,
+    })
+    async getMovie(@Request() req: UserRequest, @Param("id") _id: string) {
+        try {
+            const movie = await this.moviesService.findUserMovie({
+                userId: req.user.id,
+                _id,
+            });
+
+            if (!movie) {
+                throw new HttpException(
+                    {
+                        statusCode: HttpStatus.NOT_FOUND,
+                        message: `Couldn't find a movie with id: ${_id}`,
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }
+
+            return movie;
+        } catch (error) {
+            if (error?.kind === "ObjectId") {
+                throw new HttpException(
+                    {
+                        statusCode: HttpStatus.BAD_REQUEST,
+                        message: "Movie ID is incorrect!",
+                    },
+                    HttpStatus.BAD_REQUEST,
+                );
+            }
+
+            if (error?.response && error?.status) {
+                throw new HttpException(error.response, error.status);
+            }
+
+            throw new InternalServerErrorException();
+        }
     }
 
     @UserRoles(UserRoleEnum.Basic, UserRoleEnum.Premium)
