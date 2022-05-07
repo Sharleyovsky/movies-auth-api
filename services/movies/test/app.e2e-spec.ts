15a48a2d7398545bdec93d70c5b3e9e6610226f2
Jakub Stolarski <gamernice223@gmail.com>
2022-05-07 14:48:31 +0200

    Add unauthorized test

--- services/movies/test/app.e2e-spec.ts
+++ services/movies/test/app.e2e-spec.ts
@@ -1,38 +1,50 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { ExecutionContext, INestApplication } from "@nestjs/common";
+import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "../src/app.module";
-import { JwtGuard } from "../src/auth/guards/jwt.guard";
 import { User } from "../src/types/user";
+import { JwtService } from "@nestjs/jwt";
 
 describe("AppController (e2e)", () => {
     let app: INestApplication;
+    let jwtService: JwtService = new JwtService({
+        secret: process.env.JWT_SECRET,
+    });
     const user: User = {
         id: 1,
         role: "basic",
         name: "Jim",
     };
+    const jwtMock = jwtService.sign(
+        {
+            userId: user.id,
+            name: user.name,
+            role: user.role,
+        },
+        {
+            expiresIn: 60,
+        },
+    );
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
-        })
-            .overrideGuard(JwtGuard)
-            .useValue({
-                canActivate: (context: ExecutionContext) => {
-                    const req = context.switchToHttp().getRequest();
-                    req.user = user;
-                    return true;
-                },
-            })
-            .compile();
+        }).compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
     });
 
-    it("/movies (GET)", async () => {
+    it("/movies (GET) Unauthorized", async () => {
         const response = await request(app.getHttpServer()).get("/movies");
+
+        expect(response.status).toBe(401);
+    });
+
+    it("/movies (GET)", async () => {
+        const response = await request(app.getHttpServer())
+            .get("/movies")
+            .set("Authorization", `Bearer ${jwtMock}`);
 
         expect(response.status).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
@@ -41,7 +53,8 @@ describe("AppController (e2e)", () => {
     it("/movies (POST)", async () => {
         const response = await request(app.getHttpServer())
             .post("/movies")
-            .send({ title: "Batman" });
+            .send({ title: "Batman" })
+            .set("Authorization", `Bearer ${jwtMock}`);
 
         expect(response.status).toBe(201);
         expect(Object.keys(response.body)).toEqual([
