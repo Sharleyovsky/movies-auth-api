01c44578f0f67010385819725a15d8b890b0f501
Jakub Stolarski <gamernice223@gmail.com>
2022-05-08 16:32:20 +0200

    Add getMovies unit test

--- services/movies/src/movies/movies.spec.ts
+++ services/movies/src/movies/movies.spec.ts
@@ -1,16 +1,25 @@
 import { MoviesController } from "./movies.controller";
 import { MoviesService } from "./movies.service";
-import { Model, model } from "mongoose";
+import { Model, model, Query } from "mongoose";
 import { Movie, MovieDocument, MovieSchema } from "./movie.schema";
 import { Test, TestingModule } from "@nestjs/testing";
 import { getModelToken } from "@nestjs/mongoose";
 import { ConfigService } from "@nestjs/config";
 import { OmdbService } from "../omdb/omdb.service";
+import { UserRequest } from "../types/user";
 
 describe("movies", () => {
     let controller: MoviesController;
     let service: MoviesService;
     let mockedMovieModel: Model<MovieDocument>;
+    const user = {
+        id: 1,
+        role: "basic",
+        name: "Jim",
+    };
+    const requestMock = {
+        user,
+    } as unknown as UserRequest;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -37,5 +46,20 @@ describe("movies", () => {
         expect(controller).toBeDefined();
         expect(service).toBeDefined();
         expect(mockedMovieModel).toBeDefined();
+    });
+
+    describe("getMovies", () => {
+        it("Should return an array", async () => {
+            const query = new Query<MovieDocument[], MovieDocument>();
+            const spyFind = jest
+                .spyOn(mockedMovieModel, "find")
+                .mockReturnValue(query);
+            const spyExec = jest.spyOn(query, "exec").mockReturnValue([]);
+            const movies = await controller.getMovies(requestMock);
+
+            expect(Array.isArray(movies)).toBe(true);
+            expect(spyFind).toBeCalledTimes(1);
+            expect(spyExec).toBeCalledTimes(1);
+        });
     });
 });
