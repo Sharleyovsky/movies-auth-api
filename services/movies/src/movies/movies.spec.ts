76b64d64a89660227fc4eeccda50a673d2ca1ebc
Jakub Stolarski <gamernice223@gmail.com>
2022-05-08 17:26:54 +0200

    Add getMovie unit test

--- services/movies/src/movies/movies.spec.ts
+++ services/movies/src/movies/movies.spec.ts
@@ -7,6 +7,9 @@ import { getModelToken } from "@nestjs/mongoose";
 import { ConfigService } from "@nestjs/config";
 import { OmdbService } from "../omdb/omdb.service";
 import { UserRequest } from "../types/user";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { MoviesError } from "./movies-error.enum";
+import { ErrorDto } from "./dto/error.dto";
 
 describe("movies", () => {
     let controller: MoviesController;
@@ -60,6 +63,41 @@ describe("movies", () => {
             expect(Array.isArray(movies)).toBe(true);
             expect(spyFind).toBeCalledTimes(1);
             expect(spyExec).toBeCalledTimes(1);
+        });
+    });
+
+    describe("getMovie", () => {
+        const _id = "123";
+        const query = new Query<MovieDocument[], MovieDocument>();
+
+        it("Should return a movie", async () => {
+            const spyFindOne = jest
+                .spyOn(mockedMovieModel, "findOne")
+                .mockReturnValue(query);
+            const spyExec = jest.spyOn(query, "exec").mockReturnValue(user);
+            const movie = await controller.getMovie(requestMock, _id);
+
+            expect(movie).toEqual(user);
+            expect(spyFindOne).toBeCalledTimes(1);
+            expect(spyExec).toBeCalledTimes(1);
+        });
+
+        it("Should throw an error when movie doesn't exist in the database", async () => {
+            const spyFindOne = jest
+                .spyOn(mockedMovieModel, "findOne")
+                .mockReturnValue(query);
+            const spyExec = jest.spyOn(query, "exec").mockReturnValue(null);
+
+            try {
+                await controller.getMovie(requestMock, _id);
+                expect(spyFindOne).toBeCalledTimes(1);
+                expect(spyExec).toBeCalledTimes(1);
+            } catch (error) {
+                const errRes: ErrorDto = error?.response;
+                expect(error).toBeInstanceOf(HttpException);
+                expect(errRes.statusCode).toBe(HttpStatus.NOT_FOUND);
+                expect(errRes.message).toBe(MoviesError.NOT_FOUND);
+            }
         });
     });
 });
